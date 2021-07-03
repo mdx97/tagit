9f291f78e1f25df8012596e70373e0cee01d603d
Mathew Horner <mathewhorner456@gmail.com>
2021-07-02 19:58:31 -0500

    Added help menu and version information.

--- index.ts
+++ index.ts
@@ -4,7 +4,36 @@ import { dirname } from "https://deno.land/std@0.100.0/path/mod.ts"
 import { TagRepo } from "./src/tagRepo.ts";
 import { assertFileOp, assertInitialized, assertUninitialized, fatal } from "./src/util.ts";
 
+const VERSION = '0.1.0';
+
+export const displayHelpMenu = () => {
+    // TODO: This should probably be auto-generated based on available commands.
+    // TODO: Also we'll want to move this out of the index.ts file.
+    console.log(
+        `TagIt v${VERSION}\n` +
+        `author: Mathew Horner <mathewhorner456@gmail.com>\n\n` +
+        `Commands\n` +
+        `--------\n` +
+        `add            Adds a tag to a file.\n` +
+        `help           Displays this menu.\n` +
+        `init           Initializes the current directory as a TagIt directory.\n` +
+        `list           Lists files and their associated tags.\n` +
+        `remove         Removes a tag from a file.\n` +
+        `version        Displays the current version.\n`
+    );
+};
+
 const args = parse(Deno.args);
+
+if (args.h || args.help) {
+    displayHelpMenu();
+    Deno.exit(0);
+}
+
+if (args.v || args.version) {
+    console.log(VERSION);
+    Deno.exit(0);
+}
 
 if (!args._ || args._.length != 1) {
     console.log('Usage: deno run tagit.ts -- <command> [options]')
@@ -23,6 +52,10 @@ try {
             }
             repo.add(args.f, args.t);
             repo.save();
+            break;
+        }
+        case 'help': {
+            displayHelpMenu();
             break;
         }
         case 'init': {
@@ -64,6 +97,10 @@ try {
             const repo = await TagRepo.from('.tag');
             repo.remove(args.f, args.t);
             repo.save();
+            break;
+        }
+        case 'version': {
+            console.log(VERSION);
             break;
         }
     }
